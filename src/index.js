2f37874234024e082e737cc8ed248ccee4e25f9d
zain <iamzayn19@gmail.com>
2022-08-23 15:55:40 +0500

    Add stepNumber to game state

--- src/index.js
+++ src/index.js
@@ -65,6 +65,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: 0,
       xIsNext: !this.state.xIsNext,
     });
   }
